446eaf2cf1ebd4c7db4863ccbf5f2a0e939a6030
karlos <karlosd832@gmail.com>
2026-09-08 17:34:41 -0300

    first commit

---
+++ src/main/java/com/kdjs/dev/CadastoDeNinja/CadastoDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.kdjs.dev.CadastoDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastoDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastoDeNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdjs/dev/CadastoDeNinja/CadastoDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdjs.dev.CadastoDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastoDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
